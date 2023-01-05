1aa91a530db08681e0339f02144d20e501b53b26
riosnatalia <natalia.rios@wexinc.com>
2023-01-05 17:03:58 -0300

    chore: add Delegates annotations

---
+++ CSharpAdvanced/Delegates/PhotoProcessor.cs
@@ -0,0 +1,11 @@
+﻿namespace Delegates;
+
+public class PhotoProcessor
+{
+    public void Process(string path, Action<Photo> filterHandler)
+    {
+        var photo = Photo.Load(path);
+        filterHandler(photo);
+        photo.Save();
+    }
+}
