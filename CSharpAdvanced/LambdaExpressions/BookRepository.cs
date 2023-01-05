ce61f796f41e974b93d07bb7c35db702187bb893
riosnatalia <natalia.rios@wexinc.com>
2023-01-05 17:19:54 -0300

    chore: add Lambda Expressions annotations

---
+++ CSharpAdvanced/LambdaExpressions/BookRepository.cs
@@ -0,0 +1,18 @@
+﻿using Generics;
+
+namespace LambdaExpressions;
+
+public class BookRepository
+{
+    public List<Book> GetBooks()
+    {
+        return new List<Book>
+        {
+            new Book() { Title = "Title1", Price = 5 },
+            new Book() { Title = "Title2", Price = 7 },
+
+            new Book() { Title = "Title3", Price = 17 }
+
+        };
+    }
+}
---
+++ CSharpAdvanced/LambdaExpressions/Program.cs
@@ -0,0 +1,26 @@
+﻿using Generics;
+
+namespace LambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // args => expression
+            // number => number * number;
+
+            // No argument: () => ...
+            // Only one argument: x => ...
+            // Multiple arguments: (x, y, z) => ...
+
+            var books = new BookRepository().GetBooks();
+
+            var cheapBooks = books.FindAll(b => b.Price < 10);
+
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+        }
+    }
+}
