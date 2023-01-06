e0e732cd79272ae126e79d647f45c737b6e1536d
riosnatalia <natalia.rios@wexinc.com>
2023-01-06 10:51:31 -0300

    chore: add LINQ annotations

---
+++ CSharpAdvanced/LINQ/Book.cs
@@ -0,0 +1,7 @@
+﻿namespace LINQ;
+
+public class Book
+{
+    public string Title { get; set; }
+    public float Price { get; set; }
+}
---
+++ CSharpAdvanced/LINQ/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+
+            // LINQ Extension Methods
+            var cheapBooks = books
+                                                .Where(b => b.Price < 10)
+                                                .OrderBy(b => b.Title).
+                                                Select(b => b.Title);
+
+            var singleBook = books.SingleOrDefault(b => b.Title == "ASP.NET MVC C++");
+            
+            var firstBook = books.First(b => b.Title == "C# Advanced Topics");
+            
+            var pagedBooks = books.Skip(2).Take(3).Count();
+            
+            var maximumPrice = books.Max(b => b.Price);
+            
+            var mininumPrice = books.Min(b => b.Price);
+            
+            var sumPrices = books.Sum(b => b.Price);
+
+            var averagePrices = books.Average(b => b.Price);
+            
+            Console.WriteLine(maximumPrice);
+            Console.WriteLine(mininumPrice);
+            Console.WriteLine(sumPrices);
+            Console.WriteLine(averagePrices);
+        }
+    }
+}
