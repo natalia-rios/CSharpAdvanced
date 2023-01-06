f92a5ac80d193e43763967570d63cd1dd4847d56
riosnatalia <natalia.rios@wexinc.com>
2023-01-06 10:14:34 -0300

    chore: add Extension Methods annotations

---
+++ CSharpAdvanced/ExtensionMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be a very very long blog post blablablalblallbal";
+            var shortenedPost = post.Shorten(5);
+            Console.WriteLine(shortenedPost);
+        }
+    }
+}
