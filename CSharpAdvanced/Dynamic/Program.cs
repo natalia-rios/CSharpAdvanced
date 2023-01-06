2b69d0ea195595665ee3bf1a07b6f0dbd8ba8a77
riosnatalia <natalia.rios@wexinc.com>
2023-01-06 12:08:49 -0300

    chore: add dynamic annotations

---
+++ CSharpAdvanced/Dynamic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Mail;
+
+namespace Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic name = "Mosh";
+            name = 10;
+
+            dynamic a = 10;
+            dynamic b = 5;
+            // since a and b are dynamic, c is also dynamic
+            var c = a + b;
+           
+            //implicit conversion
+            int i = 5;
+            dynamic d = i;
+            long l = d;
+
+            dynamic excelObject = "Natalia";
+            excelObject.Optimize();
+
+
+        }
+    }
+}
