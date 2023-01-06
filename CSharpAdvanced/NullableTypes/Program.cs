87d734c2f1d9f619d01ad58614c4f752a9d36b86
riosnatalia <natalia.rios@wexinc.com>
2023-01-06 11:06:12 -0300

    chore: add Nullable annotations

---
+++ CSharpAdvanced/NullableTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime? date = null;
+            DateTime date2 = date ?? DateTime.Today;
+            DateTime? date3 = date2;
+
+            Console.WriteLine(date2);
+            Console.WriteLine(date3);
+        }
+    }
+}
